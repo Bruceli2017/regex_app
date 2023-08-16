c216b7cfe7f00f50729fa8171769291be22516ad
Bruce <abraver@livemail.tw>
2023-08-16 17:52:34 +0800

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Data2Regex
+
+## Usage
+
+```python
+streamlit run app.py
+```
+
+## Reference
+
+[EXREX](https://github.com/asciimoo/exrex)
+
+Irregular methods for regular expressions.
+
+Exrex is a command line tool and python module that generates all - or random - matching strings to a given regular expression and more. It's pure python, without external dependencies.
+
+[TDDA](https://github.com/tdda/tdda)
+
+[Streamlit](https://github.com/streamlit/streamlit)
+
+[Streamlit cheatsheet](https://cheat-sheet.streamlit.app/#display-data)
+
+[Regex 101](https://regex101.com/)
---
+++ app.py
@@ -0,0 +1,74 @@
+"""
+
+"""
+import exrex
+import streamlit as st
+from tdda import rexpy
+
+st.set_page_config(
+    page_title='Data to Regex',
+    layout='wide',
+)
+
+
+def main():
+
+    cs_body()
+
+    return None
+
+
+def cs_body():
+
+    with st.container():
+        st.header('Regex generator from data')
+        col1, col2 = st.columns(2)
+
+        # Data to regex
+        col1.subheader('Input data')
+        corpus = col1.text_area(label='input', placeholder='Paste your data', value='',
+            label_visibility='collapsed', height=50)
+        col1.code("""
+            Example input:
+            EH1 3LH
+            BB2 5NR
+            
+            Output:
+            ^[A-Z]{2}[0-9] [0-9][A-Z]{2}$
+        """)
+        print(corpus)
+        corpus = corpus.split('\n')
+        col2.subheader('Output regex')
+
+        regex_output = data2regex(corpus)
+        for regex in regex_output:
+            col2.code(regex)
+    
+    # Regex to data
+    with st.container():
+        st.header('Data generator from regex')
+        col1, col2 = st.columns(2)
+        
+        col1.subheader('Input regex')
+        regex_input = col1.text_input('Enter the regex')
+        
+        col2.subheader('Output data')
+        count = col2.number_input('How many data?', value=20)
+        output_list = []
+        for _ in range(count):
+            # exrex.getone(regex_input.encode('unicode_escape'))
+            output_list.append(exrex.getone(r'{}'.format(regex_input)))
+        if output_list:
+            col2.code('\n'.join(output_list))
+
+    st.markdown('The website will not store any input or output data. Wish it could help you')
+    st.markdown('[Contact author](mailto:abraver@livemail.tw)')
+
+
+def data2regex(corpus:list[str]) -> list:
+    result = rexpy.extract(corpus)
+    return result
+
+
+if __name__ == '__main__':
+    main()
